645d280855a5d75e790c00c3054e7ce1bf3e7b13
Dayan Sauqy <iamdayy14@gmail.com>
2026-06-01 00:18:21 +0700

    fix: exclude common image file extensions from middleware matcher path

--- middleware.ts
+++ middleware.ts
@@ -60,7 +60,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - any file with common image extensions
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
   ],
 };
